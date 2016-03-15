475c1e81ab8d4d268bf6e946556152f1ad972141
helinnet <mcoban@gmx.at>
2016-03-15 02:39:36 +0100

    classes to Core
    changed include directory

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ $documentRoot = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPAR
 /**
  * @filesource We are including the Application Class from classes directory
  */
-include_once ( $documentRoot . 'classes' . DIRECTORY_SEPARATOR . 'SPS_Application.php');
+include_once ( $documentRoot . 'Core' . DIRECTORY_SEPARATOR . 'SPS_Application.php');
 /**
  * In der folgenden Zeilen wird die Applikation in einem Try-Catch-Block ausgeführt.
  */
