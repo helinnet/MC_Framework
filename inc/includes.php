d571b942c4b8e269a2abaf1022575dd90898ec6e
helinnet <mcoban@gmx.at>
2016-03-15 02:44:24 +0100

    classes to Core
    classes dir changed to Core

--- inc/includes.php
+++ inc/includes.php
@@ -1,7 +1,7 @@
 <?php
-include_once ($realPath . 'classes' . DIRECTORY_SEPARATOR . 'SPS_Config.php');
-include_once ($realPath . 'classes' . DIRECTORY_SEPARATOR . 'SPS_ServiceContainer.php');
-include_once ($realPath . 'classes' . DIRECTORY_SEPARATOR . 'SPS_Loader.php');
+include_once ($realPath . 'Core' . DIRECTORY_SEPARATOR . 'SPS_Config.php');
+include_once ($realPath . 'Core' . DIRECTORY_SEPARATOR . 'SPS_ServiceContainer.php');
+include_once ($realPath . 'Core' . DIRECTORY_SEPARATOR . 'SPS_Loader.php');
 //Config-Objekt erzeugen
 $config = new SPS_Config();
 //Manche Einstellungen machen
@@ -17,7 +17,7 @@ $services = new SPS_ServiceContainer($config);
 
 //Loader-Objekt erzeugen
 $loader = new SPS_Loader($services);
-$loader->setDir('classes', $config->rootPath . 'classes' . DS);
+$loader->setDir('core', $config->rootPath . 'Core' . DS);
 $loader->setDir('plugins', $config->pluginsDir);
 $loader->setDir('libs', $config->libsDir);
 $loader->load();
